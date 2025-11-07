1ee0ba911b6b8e8b736aa5da4d6781c6867bcdf1
Traz0o <alan.aguilar.re@gmail.com>
2025-11-06 20:32:54 -0600

    Commite Limite

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, ImageBackground, Dimensions, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions, TouchableOpacity, ScrollView, Alert, Platform } from 'react-native';
 import { useState } from 'react';
 
 const fondo = require('./assets/fondo.jpeg');
@@ -8,7 +8,15 @@ const donjulio = require('./assets/donjulio.jpeg');
 const sopes = require('./assets/sopes.jpeg');
 const tacosduros = require('./assets/tacosduros.jpeg');
 const pizza = require('./assets/pizza.jpeg');
-const hochata = require('./assets/aguahorchata.avif');
+const hochata = require('./assets/aguadehorchata.jpeg');
+const Hamburguesa = require('./assets/hamburguesa.jpeg');
+
+const comidaobjct = [
+
+
+]
+
+
 
 
 
@@ -16,28 +24,121 @@ const hochata = require('./assets/aguahorchata.avif');
 export default function App() {
   const [ordenar, setOrdenar] = useState(false)
 
+  const tacosd = 'Tacos duros';
+  const tacosp = 'Tacos de Pastor';
+  const pissa = 'Pizza';
+  const zopes = 'Sopes';
+  const h2ojamaica = 'Agua de Jamaica';
+  const julio = 'Don Julio 70';
+  const h2ohorchata = 'Agua de Horchata';
+  const burgir = 'Hamburguesa';
+
+  const alerta = () =>{
+    if(ordenar == true){
+      if(Platform.OS === 'web'){
+        window.alert('Orden registrada correctamente');
+      }
+    }
+  }
+
+
   return (
     <ImageBackground source={fondo} style={styles.background} resizeMode="cover">
+      <ScrollView style={styles.scrollArea} contentContainerStyle={styles.scrollcontenido} showsVerticalScrollIndicator={true} >
       <View>
         <Text style={styles.header}>SonuraGrill</Text>
         <Text style={styles.headerdescription}>Restaurante de alta gama con los mejores platillos de méxico con los productos de la mejor alta calidad </Text>
       </View>
-      <ScrollView style={styles.scrollArea} contentContainerStyle={styles.scrollcontenido} showsVerticalScrollIndicator={true} >
+      
         <Text style={styles.headerdescription}>
           Platillos Especiales Mexicanos
         </Text>
 
-        <View>
-          <Image source={tacosduros} style={styles.iconos}/>
-          <Text style={style.texticonos}>Tacos Duros</Text>
-          <Text style={style.texticonos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
-          <Text style={style.texticonos}>70$</Text>
-          <TouchableOpacity styles={styles.botonordenar} onPress={() => setOrdenar(true)}>
-            <Text styles={styles.textordernar}>Ordenar</Text>
+        <View style={styles.platillos}>
+          <Image source={tacosduros} />
+          <Text style={styles.textoplatillos}>{tacosd}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
           </TouchableOpacity>
-
         </View>
 
+        <View style={styles.platillos}>
+          <Image source={pastor} />
+          <Text style={styles.textoplatillos}>{tacosp}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.platillos}>
+          <Image source={sopes} />
+          <Text style={styles.textoplatillos}>{zopes}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.headerdescription}>
+          Platillos Especiales Gringos
+        </Text>
+
+        <View style={styles.platillos}>
+          <Image source={pizza} />
+          <Text style={styles.textoplatillos}>{pissa}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.platillos}>
+          <Image source={Hamburguesa} />
+          <Text style={styles.textoplatillos}>{burgir}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.headerdescription}>
+          Bebidas
+        </Text>
+
+        <View style={styles.platillos}>
+          <Image source={jamaica} />
+          <Text style={styles.textoplatillos}>{h2ojamaica}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.platillos}>
+          <Image source={hochata} />
+          <Text style={styles.textoplatillos}>{h2ohorchata}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.platillos}>
+          <Image source={donjulio} />
+          <Text style={styles.textoplatillos}>{julio}</Text>
+          <Text style={styles.textoplatillos}>Deliciosos tacos duros con ingrediente a elegir con verdura y salsa</Text>
+          <Text style={styles.textoplatillos}>70$</Text>
+          <TouchableOpacity style={styles.botonordenar} onPress={() => alerta()}>
+            <Text style={styles.textordernar}>Ordenar</Text>
+          </TouchableOpacity>
+        </View>
 
 
 
@@ -78,7 +179,29 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
 
   },
-  iconos:{
+  platillos: {
+    flex: 1,
+    backgroundColor: '#a2a2a2ff',
+    paddingVertical: 10
+  },
+  botonordenar: {
+    backgroundColor: 'white',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  textordernar: {
+    textAlign: 'center',
+  },
+  textoplatillos:{
+    fontSize: 10,
+    color: 'white',
+    lineHeight: 24,
+    textAlign: 'justify',
+    marginBottom: 20,
+    padding: 10,
+
 
   },
 
